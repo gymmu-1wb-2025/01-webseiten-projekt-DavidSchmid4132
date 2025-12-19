ee5b221b940809ff54e22f0e63a6352fdeb61375
David Schmid <e228399@edu.sbl.ch>
2025-12-19 14:56:44 +0000

    Icon und Kommentare beendet

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let showNumer = false;
 function increaseCount() {
   let titleElement = document.querySelector("h1");
 
-  //
+  // Kehrt den aktuellen Wert von showNumer um: ist es true, wird es false, und umgekehrt
   showNumer = !showNumer;
 
-  //
+  // Wenn showNumer true ist, wird die Zahl 17 angezeigt, sonst der Text "Die Karriere von Josh Allen"
   if (showNumer) {
     let count = 17;
     titleElement.textContent = count;
